e8011a60a32f7839ea363d43e687f425f81eea6a
migueldr12 <253625159+IDGS-802-21002866@users.noreply.github.com>
2026-01-22 14:48:47 -0600

    operasBas

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template
-from flask.cli import F
+from ast import match_case
+from flask import Flask, render_template, request
 
 
 app = Flask(__name__)
@@ -64,7 +64,52 @@ def operas():
             <input type="text" id="apellidoPaterno" name="apellidoPaterno" required>
         </form>
     """
+@app.route('/operasBas', methods=["GET", "POST"])
+def operasBas():
+    n1 = request.form.get("n1")
+    n2 = request.form.get("n2")
+    operacion = request.form.get("operacion")
 
+    match(operacion):
+        case "suma":
+            res = float(n1) + float(n2)
+        case "resta":   
+            res = float(n1) - float(n2)
+        case "multiplicacion":
+            res = float(n1) * float(n2) 
+        case "division":
+            if float(n2) != 0:
+                res = float(n1) / float(n2)
+            else:
+                res = "Error: División por cero no permitida."
+        case _:
+            res = ""
+
+    return render_template("operasBas.html", n1=n1, n2=n2, res=res)
+
+@app.route('/resultado', methods=["GET", "POST"])
+def resultado():
+    n1 = request.form.get("n1")
+    n2 = request.form.get("n2")
+    operacion = request.form.get("operacion")
+
+    match(operacion):
+        case "suma":
+            return f"La suma de {n1} y {n2} es: {float(n1) + float(n2)}"
+        case "resta":
+            return f"La resta de {n1} y {n2} es: {float(n1) - float(n2)}"
+        case "multiplicacion":
+            return f"La multiplicación de {n1} y {n2} es: {float(n1) * float(n2)}"
+        case "division":
+            if float(n2) != 0:
+                return f"La división de {n1} y {n2} es: {float(n1) / float(n2)}"
+            else:
+                return "Error: División por cero no permitida."
+        case _:
+            return "Operación no válida."
+
+
+    return f"La suma de {n1} y {n2} es: {float(n1) + float(n2)}"
 
 if __name__ == "__main__":
     app.run(debug=True)
